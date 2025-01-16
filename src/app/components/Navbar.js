deb7c5f16bb52758604c64b15cf6674fecc1532d
Lucas Vasconcelos <lucasvasconcelos503@gmail.com>
2025-01-16 10:39:47 -0500

    added file download link in hero text

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -32,7 +32,7 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="flex w-full h-10 justify-center items-center fixed mt-2">
+        <nav className="flex w-full h-10 justify-center items-center fixed mt-2 z-50">
             <div className='w-1/4 h-10 grid grid-cols-3 font-mono rounded-xl justify-center items-center border-2 border-blue-500'>
                 <div className='flex justify-center items-center transform transition-all ease-in-out hover:scale-110 hover:text-green-300'>
                     <Link id={0} href="#Home" onClick={() => handleClick(0)}>Home</Link>
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,42 @@
+'use client'
 import ProjectCard from './components/ProjectCard';
-import Image from 'next/image';
 
 export default function HomePage() {
+
+  async function downloadResume() {
+    try {
+      const response = await fetch('/api/downloadResume')
+
+      // Create a temporary anchor element to trigger the download
+      const url = window.URL.createObjectURL(await response.blob());
+      const link = document.createElement("a");
+      link.href = url;
+      
+      // Setting filename received in response
+      link.setAttribute("download", 'Lucas Vasconcelos Resume 2025.pdf');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+    } catch (error) {
+      console.error("Error downloading file:", error);
+    }
+  };
+
   return (
     <>
       <section id="Home" className="h-96 w-full flex justify-center items-center mb-10">
         <video autoPlay muted loop className="w-full h-full object-cover blur-md -z-10">
           <source src='/videos/Hardwood_Floor_KneeSlide_0125.MOV'></source>
         </video>
-        {/* <p>
-          Hi there! Thanks for visiting my site! I am actively seeking employment as a Software Engineer so if you or
-          anyone you know could use my skillsets highlighted in the projects linked below, please share my resume and contact information with them!
-        </p> */}
+        <div className='absolute'>
+          <p>
+            Hi there! Thanks for visiting my site! I am actively seeking employment as a Software Engineer so if you or
+            anyone you know could use my skillsets highlighted in the projects linked below, please <button onClick={downloadResume} className='underline'>download my resume</button> and share it with them!
+          </p>
+        </div>
       </section>
-      <section id="Projects" className='w-full h-auto grid grid-rows-2 grid-cols-2 gap-x-5 gap-y-5 justify-items-center items-center'>
+      <section id="Projects" className='w-full h-auto grid grid-cols-4 place-items-center'>
         <ProjectCard projectLink='https://github.com/LucasVasconcelos10/photography-salesman' cardImgSrc='/logos/github-mark.png' cardImgAlt='Github Logo' />
         <ProjectCard projectLink='https://github.com/LucasVasconcelos10/photography-salesman' cardImgSrc='/logos/github-mark.png' cardImgAlt='Github Logo' />
         <ProjectCard projectLink='https://github.com/LucasVasconcelos10/photography-salesman' cardImgSrc='/logos/github-mark.png' cardImgAlt='Github Logo' />
